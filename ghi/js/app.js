2ce024a1aa6e7d1a11abc8529d7241c637988123
spiderthatcodes <jessicaldickerson@outlook.com>
2023-10-09 10:59:21 -0700

    conference card doneish

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,4 +1,29 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
+    const url = 'http://localhost:8000/api/conferences/';
 
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        // Figure out what to do when the response is bad
+    } else {
+        const data = await response.json();
+
+        const conference = data.conferences[0];
+        const nameTag = document.querySelector('.card-title');
+        nameTag.innerHTML = conference.name;
+
+        const detailUrl = `http://localhost:8000${conference.href}`;
+        const detailResponse = await fetch(detailUrl);
+        if (detailResponse.ok) {
+          const details = await detailResponse.json();
+          const detailTag = document.querySelector('.card-text')
+          detailTag.innerHTML = details.conference.description
+        }
+
+      }
+    } catch (e) {
+      // Figure out what to do if an error is raised
+    }
 
 });
